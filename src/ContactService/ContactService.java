d7ba04a57bdb7dfaa6bfea1296006a077cd5b70b
Troy Smith <troy.smith1@snhu.edu>
2021-06-09 21:38:08 +0000

    UPDATE ContactService.java & ContactServiceTest.java
    I updated both without errors and 100% coverage.

--- src/ContactService/ContactService.java
+++ src/ContactService/ContactService.java
@@ -1,54 +1,72 @@
 package ContactService;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import Contact.Contact;
 
 public class ContactService {
-	public static int uniqueId=2000;
-	   public static Map<Integer, Contact> contacts = new HashMap<>();
-	   
-	public Map<Integer, Contact> addContact(Contact contact) {
-	       uniqueId++;
-	       contacts.put(uniqueId, contact);
-	       return contacts;
-	   }
-	public Map<Integer, Contact> deleteContact(String contactID) {
-
-	       Iterator<Entry<Integer, Contact>> itr = contacts.entrySet().iterator();
-
-	       while (itr.hasNext()) {
-	           Map.Entry<Integer, Contact> entry = (Map.Entry<Integer, Contact>) itr.next();
-	           if (entry.getValue().getcontactID() == contactID) {
-	               itr.remove();
-	           }
-	       }
-
-	       return contacts;
-
-	   }
-	 public Map<Integer, Contact> updateContact(String contactID, String firstName, String lastName, String Number,
-	           String Address) {
-
-	       Iterator<Entry<Integer, Contact>> itr = contacts.entrySet().iterator();
-
-	       while (itr.hasNext()) {
-	           Map.Entry<Integer, Contact> entry = (Map.Entry<Integer, Contact>) itr.next();
-
-	           if (entry.getValue().getcontactID() == contactID) {
-	               entry.getValue().setfirstName(firstName);
-	               entry.getValue().setlastName(lastName);
-	               entry.getValue().setNumber(Number);
-	               entry.getValue().setAddress(Address);
-	           }
-
-	       }
-
-	       return contacts;
-
-	   }
-
-}
+	
+ private String uniqueId;
+ public List<Contact> contactList = new ArrayList<>();
+ {
+ uniqueId = UUID.randomUUID().toString().substring(
+ 0, Math.min(toString().length(), 10));
+ }
+ public void newContact() {
+ Contact contact = new Contact(newUniqueId());
+ contactList.add(contact);
+ }
+ public void newContact(String firstname) {
+ Contact contact = new Contact(newUniqueId(), firstname);
+ contactList.add(contact);
+ }
+ public void newContact(String firstname, String lastname) {
+ Contact contact = new Contact(newUniqueId(), firstname, lastname);
+ contactList.add(contact);
+ }
+ public void newContact(String firstname, String lastname,
+ String phonenumber) {
+ Contact contact =
+ new Contact(newUniqueId(), firstname, lastname, phonenumber);
+ contactList.add(contact);
+ }
+ public void newContact(String firstname, String lastname, String phonenumber,
+ String address) {
+ Contact contact =
+ new Contact(newUniqueId(), firstname, lastname, phonenumber, address);
+ contactList.add(contact);
+ }
+ public void deleteContact(String id) throws Exception {
+ contactList.remove(searchForContact(id));
+ }
+ public void updateFirstName(String id, String firstName) throws Exception {
+ searchForContact(id).updateFirstName(firstName);
+ }
+ public void updateLastName(String id, String lastName) throws Exception {
+ searchForContact(id).updateLastName(lastName);
+ }
+ public void updatePhoneNumber(String id, String phoneNumber)
+ throws Exception {
+ searchForContact(id).updatePhoneNumber(phoneNumber);
+ }
+ public void updateAddress(String id, String address) throws Exception {
+	 searchForContact(id).updateAddress(address);
+	 }
+	 public List<Contact> getContactList() { return contactList; }
+	 private String newUniqueId() {
+	 return uniqueId = UUID.randomUUID().toString().substring(
+	 0, Math.min(toString().length(), 10));
+	 }
+	 private Contact searchForContact(String id) throws Exception {
+	 int index = 0;
+	 while (index < contactList.size()) {
+	 if (id.equals(contactList.get(index).getContactId())) {
+	 return contactList.get(index);
+	 }
+	 index++;
+	 }
+	 throw new Exception("The Task does not exist!");
+	 }
+	}
